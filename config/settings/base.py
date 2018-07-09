422173b865c9c36f75e68b3028bd8f5818aa7a33
yshalivskyy <yaroslav@shalivsky.com>
2018-07-09 15:37:32 +0300

    update requirements

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,9 +65,6 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     'crispy_forms',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
     'rest_framework',
     'webpack_loader',
     'corsheaders'
