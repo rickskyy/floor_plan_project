a1f3e3de72350e9cdb0a0ebe14b71d6c2d1adf28
yshalivskyy <yaroslav@shalivsky.com>
2018-07-09 21:04:16 +0300

    resolving product build envs 2

--- floor-plans-vuejs-app/src/http/APIService.js
+++ floor-plans-vuejs-app/src/http/APIService.js
@@ -3,32 +3,32 @@ const API_URL = process.env.ROOT_API
 
 export class APIService {
   getImageRecords (page) {
-    const url = `${API_URL}/image_records/?page=` + page
+    const url = `${API_URL}image_records/?page=` + page
     return axios.get(url).then(response => response.data)
   }
 
   getImageRecord (pk) {
-    const url = `${API_URL}/image_records/${pk}`
+    const url = `${API_URL}image_records/${pk}`
     return axios.get(url).then(response => response.data)
   }
 
   getClassifications () {
-    const url = `${API_URL}/classifications/`
+    const url = `${API_URL}classifications/`
     return axios.get(url).then(response => response.data)
   }
 
   getClassification (pk) {
-    const url = `${API_URL}/classifications/${pk}`
+    const url = `${API_URL}classifications/${pk}`
     return axios.get(url).then(response => response.data)
   }
 
   createImageRecord (data, config) {
-    const url = `${API_URL}/image_records/`
+    const url = `${API_URL}image_records/`
     return axios.post(url, data, config)
   }
 
   createClassification (classification) {
-    const url = `${API_URL}/classifications/`
+    const url = `${API_URL}classifications/`
     return axios.post(url, classification)
   }
 
